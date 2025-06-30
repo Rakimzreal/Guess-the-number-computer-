e1b6836f39b7a2adccb51a6a32950892ae35661d
Kay <126708934+Rakimzreal@users.noreply.github.com>
2025-06-30 03:54:58 +0200

    Add restart to the game function

--- game.py
+++ game.py
@@ -2,16 +2,21 @@ import random
 
 
 def guess():
-    x = int(input('Enter a maximum value: '))
-    random_number = random.randint(1, x)
-    guess = 0
-    while guess != random_number:
-        guess = int(input(f'Guess a number between 1 and {x}: '))
-        if guess < random_number:
-            print('Try again, Too low! ')
-        elif guess > random_number:
-            print('Try again, Too high! ')
+    while True:
+        x = int(input('Enter a maximum value: '))
+        random_number = random.randint(1, x)
+        guess = 0
+        while guess != random_number:
+            guess = int(input(f'Guess a number between 1 and {x}: '))
+            if guess < random_number:
+                print('Try again, Too low! ')
+            elif guess > random_number:
+                print('Try again, Too high! ')
 
-    print(f'You guessed the number {random_number} correctly!! ')
+        print(f'You guessed the number {random_number} correctly!! ')
+
+        choice = input('Restart? (yes/no): ')
+        if choice != 'yes':
+            break
     
 guess()
