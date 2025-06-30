6848debc765d2991f98b1235a6a720949a8ee5cb
Kay <126708934+Rakimzreal@users.noreply.github.com>
2025-06-30 03:59:38 +0200

    Add, print the number of attempts

--- game.py
+++ game.py
@@ -6,8 +6,11 @@ def guess():
         x = int(input('Enter a maximum value: '))
         random_number = random.randint(1, x)
         guess = 0
+        attempts = 0
         while guess != random_number:
             guess = int(input(f'Guess a number between 1 and {x}: '))
+            attempts +=1
+            print(f'Tries: ', attempts)
             if guess < random_number:
                 print('Try again, Too low! ')
             elif guess > random_number:
@@ -15,7 +18,7 @@ def guess():
 
         print(f'You guessed the number {random_number} correctly!! ')
 
-        choice = input('Restart? (yes/no): ')
+        choice = input('Restart? (yes/no): ').lower().strip()
         if choice != 'yes':
             break
     
