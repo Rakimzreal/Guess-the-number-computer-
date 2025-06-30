0c09b19a02bf305ba11c6e2e766f24009a1a9c34
Kay <126708934+Rakimzreal@users.noreply.github.com>
2025-06-30 04:20:43 +0200

    Check for possible errror and print invalid if user types strings

--- game.py
+++ game.py
@@ -3,12 +3,19 @@ import random
 
 def guess():
     while True:
-        x = int(input('Enter a maximum value: '))
+        try:
+            x = int(input('Enter a maximum value: '))
+        except ValueError:
+            print('Invalid input! ')
+            continue
         random_number = random.randint(1, x)
         guess = 0
         attempts = 0
         while guess != random_number:
-            guess = int(input(f'Guess a number between 1 and {x}: '))
+            try:
+                guess = int(input(f'Guess a number between 1 and {x}: '))
+            except ValueError:
+                print('Invalid input! ')
             attempts +=1
             print(f'Tries: ', attempts)
             if guess < random_number:
