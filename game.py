36b2b8cc067e821a0b7b077717d2d36803371fbe
Kay <126708934+Rakimzreal@users.noreply.github.com>
2025-06-30 03:51:46 +0200

    Write the game function

--- game.py
+++ game.py
@@ -1,4 +1,17 @@
 import random
 
-random_number = random.randint()
 
+def guess():
+    x = int(input('Enter a maximum value: '))
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        if guess < random_number:
+            print('Try again, Too low! ')
+        elif guess > random_number:
+            print('Try again, Too high! ')
+
+    print(f'You guessed the number {random_number} correctly!! ')
+    
+guess()
